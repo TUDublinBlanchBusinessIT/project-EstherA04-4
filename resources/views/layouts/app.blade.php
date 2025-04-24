f36b3a78427b5ad0fb27bc1a0820bf984ebae61f
unknown <estherakinlade04@gmail.com>
2025-04-24 21:18:26 +0100

    created new file

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Football Manager</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/">Football Manager</a>
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item"><a class="nav-link" href="{{ route('teams.index') }}">Teams</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('players.index') }}">Players</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    @yield('content')
+</body>
+</html>
