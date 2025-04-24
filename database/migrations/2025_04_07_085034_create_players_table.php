eb2dabb46701c60f31b8bcf681e2f53f404951c7
unknown <estherakinlade04@gmail.com>
2025-04-24 18:04:06 +0100

    made changes to players table

--- database/migrations/2025_04_07_085034_create_players_table.php
+++ database/migrations/2025_04_07_085034_create_players_table.php
@@ -4,28 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePlayersTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
+return new class extends Migration {
+    public function up(): void {
         Schema::create('players', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('position');
+            $table->foreignId('team_id')->constrained()->onDelete('cascade');
+            $table->integer('age');
+            $table->string('nationality');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
+    public function down(): void {
         Schema::dropIfExists('players');
     }
-}
+};
