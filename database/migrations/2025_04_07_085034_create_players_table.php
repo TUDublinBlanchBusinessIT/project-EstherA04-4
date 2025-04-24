098d7c7ec33903bf942987acc410624f5f609086
unknown <estherakinlade04@gmail.com>
2025-04-24 20:56:43 +0100

    got something working

--- database/migrations/2025_04_07_085034_create_players_table.php
+++ database/migrations/2025_04_07_085034_create_players_table.php
@@ -4,20 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
-    public function up(): void {
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
         Schema::create('players', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('position');
-            $table->foreignId('team_id')->constrained()->onDelete('cascade');
-            $table->integer('age');
-            $table->string('nationality');
-            $table->timestamps();
+            $table->id(); // Primary Key
+            $table->string('name'); // Player name
+            $table->foreignId('team_id')->constrained()->onDelete('cascade'); // FK to teams
+            $table->timestamps(); // created_at and updated_at
         });
     }
 
-    public function down(): void {
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
         Schema::dropIfExists('players');
     }
 };
