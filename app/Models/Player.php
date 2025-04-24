d1dd5cdafcdc57a5ebbe7d8f125e6f3235141831
unknown <estherakinlade04@gmail.com>
2025-04-24 18:07:04 +0100

    created players models

--- app/Models/Player.php
+++ app/Models/Player.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Player extends Model
-{
+class Player extends Model {
     use HasFactory;
+
+    protected $fillable = ['name', 'position', 'team_id', 'age', 'nationality'];
+
+    public function team() {
+        return $this->belongsTo(Team::class);
+    }
 }
