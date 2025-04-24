392298b9d3734efba92adb4ab43bab490302a0a7
unknown <estherakinlade04@gmail.com>
2025-04-24 20:56:01 +0100

    got something working

--- database/migrations/2025_04_07_085023_create_teams_table.php
+++ database/migrations/2025_04_07_085023_create_teams_table.php
@@ -4,18 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
-    public function up(): void {
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
         Schema::create('teams', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('country');
-            $table->integer('founded_year');
-            $table->timestamps();
+            $table->id(); // Primary Key
+            $table->string('name'); // Team name
+            $table->timestamps(); // created_at and updated_at
         });
     }
 
-    public function down(): void {
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
         Schema::dropIfExists('teams');
     }
 };
